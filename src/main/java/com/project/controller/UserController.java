468c47168f8de937f0f542f19585c207a85f2318
Chayan Barman <barmanchayan10@gmail.com>
2024-06-25 02:03:20 +0530

    Entity Items Created, Repo,Service added

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -1,18 +1,46 @@
 package com.project.controller;
 
+import com.project.entity.user.Posts;
+import com.project.entity.user.User;
+import com.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/user")
     public ResponseEntity<?> userHome(){
         return new ResponseEntity<>("From User Home", HttpStatus.OK);
     }
+
+    @PostMapping("/user/saveUser")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        try {
+            User createdUser = userService.createUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/user/savePost")
+    public ResponseEntity<String> createPost(@RequestBody Posts post, @RequestParam Long diaryId) {
+        try {
+            Posts createdPost = userService.createPost(diaryId, post);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Post Creation Successfull...");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // Or handle specific exception
+        }
+    }
+
+
 }
--- src/main/java/com/project/entity/user/User.java
+++ src/main/java/com/project/entity/user/User.java
@@ -1,6 +1,10 @@
-package com.project.entity;
+package com.project.entity.user;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -8,22 +12,35 @@ import java.sql.Timestamp;
 import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "user_id", updatable = false, nullable = false)
     private UUID userId;
+
+    @OneToOne(mappedBy = "user",cascade = CascadeType.ALL)
+    private UserDiary diary;
+
     @Column(name = "user_name", nullable = false)
     private String userName;
+
     @Column(name = "user_email", nullable = false)
     private String userEmail;
+
     @Column(name = "user_password", nullable = false)
     private String userPassword;
+
     @Column(name = "user_profile_image")
     private String userProfileImage;
+
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private Timestamp createAt;
+
     @UpdateTimestamp
     @Column(name = "updated_at")
     private Timestamp updatedAt;
